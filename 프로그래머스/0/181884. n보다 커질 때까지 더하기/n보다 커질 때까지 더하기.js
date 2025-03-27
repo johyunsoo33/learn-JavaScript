8c7a7989e93630a8dc56a7fd07bb01a24d7cc730
J.H.S_0616 <johyunsoo369@naver.com>
2025-03-27 18:12:56 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers, n) {
+    var answer = 0;
+    var sum = 0;
+    for(let i=0; i<numbers.length; i++){
+        sum += numbers[i];
+        if(sum > n){
+            return answer = sum
+        }
+    }
+    return answer;
+}
