2435d90c2feb877add5ef6e633de44642930fec5
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-07 16:08:20 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,8 +1,19 @@
-const fs = require("fs");
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  console.log(data.a + data.b);
+}
+main();
 
-const input = fs.readFileSync("/dev/stdin").toString().split(' ');
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString().trim().split(" ");
 
-const A = parseInt(input[0]);
-const B = parseInt(input[1]);
+  const result = new Object();
 
-console.log(A + B);
+  result.a = parseInt(fileData[0]); // result.a = 10
+  result.b = parseInt(fileData[1]); // result.b = 20
+  // console.log(result.a, result.b);
+  return result;
+}
+getData();
