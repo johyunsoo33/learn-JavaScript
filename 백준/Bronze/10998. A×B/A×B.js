20b766281e9f1b3d47df6cc293fa4e14c810d2fa
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-07 16:09:26 +0900

    [Bronze V] Title: A×B, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,19 @@
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  console.log(data.a * data.b);
+}
+main();
+
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString().trim().split(" ");
+
+  const result = new Object();
+
+  result.a = parseInt(fileData[0]); // result.a = 10
+  result.b = parseInt(fileData[1]); // result.b = 20
+  // console.log(result.a, result.b);
+  return result;
+}
+getData();
