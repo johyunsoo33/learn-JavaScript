5dea1d5193b1cb1d0482eeb0e3d53ccad58be755
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-08 11:29:30 +0900

    [Bronze V] Title: 사분면 고르기, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -1,17 +1,29 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split("\n");
+function main() {
+  const data = getData();
+  let x = data.a;
+  let y = data.b;
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  if (x > 0 && y > 0) {
+    console.log("1");
+  } else if (x < 0 && y > 0) {
+    console.log("2");
+  } else if (x < 0 && y < 0) {
+    console.log("3");
+  } else if (x > 0 && y < 0) {
+    console.log("4");
+  } else {
+    console.log("0");
+  }
+}
+main();
 
-const x = parseInt(input[0]);
-const y = parseInt(input[1]);
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString().trim().split("\n");
 
-if (x > 0 && y > 0) {
-  console.log("1");
-} else if (x < 0 && y > 0) {
-  console.log("2");
-} else if (x < 0 && y < 0) {
-  console.log("3");
-} else if (x > 0 && y < 0) {
-  console.log("4");
-} else {
-  console.log("0");
+  const result = new Object();
+
+  result.a = isNaN(fileData[0]) ? fileData[0] : parseInt(fileData[0]);
+  result.b = isNaN(fileData[1]) ? fileData[1] : parseInt(fileData[1]);
+  return result;
 }
