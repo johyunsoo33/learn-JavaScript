9bdf4fd72a0157bceae4c9d8f69891f22465bf3f
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-08 10:47:25 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -1,13 +1,23 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  if (data.a > data.b) {
+    console.log(">");
+  } else if (data.a < data.b) {
+    console.log("<");
+  } else {
+    console.log("==");
+  }
+}
+main();
 
-const A = Number(input[0]);
-const B = Number(input[1]);
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString().trim().split(" ");
 
-if(A > B) {
-   console.log(">");
-} else if(A < B) {
-   console.log("<");
-} else {
-   console.log("==");
+  const result = new Object();
+
+  result.a = isNaN(fileData[0]) ? fileData[0] : parseInt(fileData[0]);
+  result.b = isNaN(fileData[1]) ? fileData[1] : parseInt(fileData[1]);
+  return result;
 }
