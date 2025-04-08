0179a1e527a9eae5a53c49f989309dd9d2ecd480
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-08 10:48:49 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -1,7 +1,17 @@
-const fs = require('fs');
-const input=fs.readFileSync(0).toString().trim().split(" ");
-const a = parseInt(input[0]); 
-const b = parseInt(input[1]); 
-const c = parseInt(input[2]); 
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  console.log(data.a + data.b + data.c);
+}
+main();
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString().trim().split(" ");
 
-console.log(a+b+c);
+  const result = new Object();
+
+  result.a = isNaN(fileData[0]) ? fileData[0] : parseInt(fileData[0]);
+  result.b = isNaN(fileData[1]) ? fileData[1] : parseInt(fileData[1]);
+  result.c = isNaN(fileData[2]) ? fileData[2] : parseInt(fileData[2]);
+  return result;
+}
