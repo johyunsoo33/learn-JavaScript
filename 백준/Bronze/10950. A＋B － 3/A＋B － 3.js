ef0936c6b61ce5904ad985e27e9573599b518a97
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-10 10:46:33 +0900

    [Bronze V] Title: A+B - 3, Time: 100 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,8 +1,23 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split("\n");
-let testNum = parseInt(input[0]);
-for (let i = 1; i <= testNum; i++) {
-  let a = parseInt(input[i].split(" ")[0]);
-  let b = parseInt(input[i].split(" ")[1]);
-  console.log(a + b);
+    function main() {
+  const data = getData();
+  for (let i = 1; i < data.length; i++) {
+    const rowArr = data[i];
+    console.log(rowArr[0] + rowArr[1]);
+  }
+}
+main();
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const row = arr[i];
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr);
+  }
+  return result;
 }
