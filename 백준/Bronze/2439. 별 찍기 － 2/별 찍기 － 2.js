3e836f8f49c570334f4087abb5608c8dcb9f3e6f
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-10 14:20:26 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 96 ms, Memory: 9544 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -1,12 +1,26 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split(" ");
-let answer = "";
-let blank = "";
-for (let i = 1; i <= input; i++) {
-  answer += "*";
-  for (let j = 0; j < input - i; j++) {
-    blank += " ";
+function main() {
+  const data = getData();
+  let blank = "";
+  let stars = "";
+  for (let i = 1; i <= data; i++) {
+    blank = " ".repeat(data - i);
+    stars = "*".repeat(i);
+    console.log(blank + stars);
   }
-  console.log(blank + answer);
-  blank = "";
+}
+main();
+
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+  const result = [];
+  for (let row of arr) {
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr.length === 1 ? rowArr[0] : rowArr);
+  }
+  return result.length === 1 ? result[0] : result;
 }
