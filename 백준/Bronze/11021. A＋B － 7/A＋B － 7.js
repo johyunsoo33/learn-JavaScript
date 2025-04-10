4386269fcbb7796d6de979cdea77e0e283ec9e53
J.H.S_0616 <johyunsoo369@naver.com>
2025-04-10 13:23:53 +0900

    [Bronze V] Title: A+B - 7, Time: 92 ms, Memory: 9512 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -1,8 +1,23 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split("\n");
-let testNum = parseInt(input[0]);
-for (let i = 1; i <= testNum; i++) {
-  let a = parseInt(input[i].split(" ")[0]);
-  let b = parseInt(input[i].split(" ")[1]);
-  console.log(`Case #${i}:` + " " + (a + b));
+function main() {
+  const data = getData();
+  for (let i = 1; i < data.length; i++) {
+    console.log(`Case #${i}: ${data[i][0] + data[i][1]}`);
+  }
+}
+main();
+
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const row = arr[i];
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr);
+  }
+  return result;
 }
