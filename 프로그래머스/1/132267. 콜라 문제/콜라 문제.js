45f9b531881b6ac25693e041689bb9eeccf0b3ab
J.H.S_0616 <johyunsoo369@naver.com>
2026-01-21 14:31:45 +0900

    [level 1] Title: 콜라 문제, Time: 0.83 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,27 @@
+//반복문
+// function solution(a, b, n) {
+//     var answer = 0;
+//     let currentBottles = n;
+//     while(currentBottles>=a){
+//         const exchange_cola = currentBottles / a
+//         const increaseBottleCount = Math.floor(exchange_cola) * b
+//         const empty_bottle = currentBottles % a;
+//         answer += increaseBottleCount
+//         currentBottles = increaseBottleCount + empty_bottle;
+//     }
+//     return answer;
+// }
+
+//재귀함수
 function solution(a, b, n) {
-    var answer = 0;
-    let currentBottles = n;
-    while(currentBottles>=a){
-        const exchange_cola = currentBottles / a
-        const increaseBottleCount = Math.floor(exchange_cola) * b
-        const empty_bottle = currentBottles % a;
-        answer += increaseBottleCount
-        currentBottles = increaseBottleCount + empty_bottle;
+    if(n<a){
+        return 0;
     }
-    return answer;
+    var answer = 0;
+    
+    const exChangeCola = Math.floor(n / a)*b
+    const remainBottle = n % a;
+    
+    
+    return exChangeCola + solution(a,b,exChangeCola+remainBottle);
 }
