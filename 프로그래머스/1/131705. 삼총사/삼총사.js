9385c7841ca37f90144b2672bf9e5c37e827eef1
J.H.S_0616 <johyunsoo369@naver.com>
2026-01-21 15:40:03 +0900

    [level 1] Title: 삼총사, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,13 +1,37 @@
+//일반 반복문
+// function solution(number) {
+//     var answer = 0;
+//     for(let i =0; i<number.length; i++){
+//         for(let j = i+1; j<number.length; j++){
+//             for(let k=j+1; k<number.length; k++){
+//                 if(number[i]+number[j]+number[k] === 0){
+//                     answer += 1;
+//                 }
+//             }
+//         }
+//     }
+//     return answer;
+// }
+
+//재귀함수 시도
 function solution(number) {
     var answer = 0;
-    for(let i =0; i<number.length; i++){
-        for(let j = i+1; j<number.length; j++){
-            for(let k=j+1; k<number.length; k++){
-                if(number[i]+number[j]+number[k] === 0){
-                    answer += 1;
-                }
+    function findCombination(first,second,last){
+        if(last < number.length){
+            if (number[first]+number[second]+number[last] === 0){
+                answer += 1;
             }
+            findCombination(first,second,last+1);
+        }
+        else if (second < number.length-2){
+         
+            findCombination(first,second+1,second+2);
+        }
+        else if(first < number.length-3){
+            
+            findCombination(first+1,first+2,first+3);
         }
     }
+    findCombination(0,1,2)
     return answer;
 }
