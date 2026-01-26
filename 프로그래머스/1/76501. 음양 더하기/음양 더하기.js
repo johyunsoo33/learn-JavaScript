09e9f4288700644309439972a34532ce6b52e257
J.H.S_0616 <johyunsoo369@naver.com>
2026-01-26 16:18:54 +0900

    [level 1] Title: 음양 더하기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,12 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    for(let i = 0; i<absolutes.length; i++){
+            if(signs[i] === false){
+            answer += absolutes[i] * -1;
+        } else {
+            answer += absolutes[i];
+        }
+        }
+        return answer;
+    }
+
