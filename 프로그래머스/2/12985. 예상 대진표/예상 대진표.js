214a7644256d388e2480564a778f6f3772c7309b
J.H.S_0616 <johyunsoo369@naver.com>
2026-02-26 16:04:36 +0900

    [level 2] Title: 예상 대진표, Time: 410.14 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,36 +1,41 @@
-// function solution(n,a,b)
-// {
-//     var answer = 1;
-//     const mustSelect = [a,b];
-//     let foundAB = false;
-//     let arr1 = Array.from({length: n/2}, (_, i) => [i * 2 + 1, i * 2 + 2]);
-//     while(foundAB === false){
-//         answer++; 
-//         const select = arr1.map((winner)=>{
-//             const found = winner.find(num => mustSelect.includes(num));
-//                 if(found) {
-//                     return found;
-//                 }
-//             return winner[Math.floor(Math.random() * 2)];
-//             }
-//         );
+function solution(n, a, b) {
+    var answer = 0;
+    let arr1 = Array.from({length: n/2}, (_, i) => [i * 2 + 1, i * 2 + 2]);
+    for(let pair of arr1) {
+        if(pair.includes(a) && pair.includes(b)) {
+            return 1;
+        }
+    }
+    
+    while(true) {
+        answer++; 
         
-//         for(let i = 0; i < select.length - 1; i += 2) {
-//             const pair = [select[i], select[i + 1]];
-//             if(pair.includes(a) && pair.includes(b)) {
-//                 foundAB = true;
-//                 break;
-//             }
-//         }
-//         if(foundAB) break;
-//         const result = [];
-//         for(let i=0; i<select.length - 1; i+=2){
-//             result.push([select[i], select[i + 1]]);
-//         }
-//         arr1 = result;
-//     }
-//     return answer;
-// }
-
-
-solution=(n,a,b)=>((a - 1) ^ (b - 1)).toString(2).length;
+        const select = arr1.map((winner) => {
+            if(winner.includes(a)) return a;
+            if(winner.includes(b)) return b;
+            return winner[0];
+        });
+        
+        for(let i = 0; i < select.length; i += 2) {
+            if(i + 1 < select.length) {
+                if((select[i] === a && select[i+1] === b) || 
+                   (select[i] === b && select[i+1] === a)) {
+                    return answer + 1;
+                }
+            }
+        }
+        
+        const result = [];
+        for(let i = 0; i < select.length; i += 2) {
+            if(i + 1 < select.length) {
+                result.push([select[i], select[i + 1]]);
+            }
+        }
+        
+        if(result.length === 0) break;
+        
+        arr1 = result;
+    }
+    
+    return answer;
+}
