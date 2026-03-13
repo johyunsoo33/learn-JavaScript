92d601804f6b0437abb67d76a7c860b94fcbc790
J.H.S_0616 <johyunsoo369@naver.com>
2026-03-13 16:13:14 +0900

    [level 1] Title: 소수 만들기, Time: 4.83 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,22 @@
+function solution(nums) {
+    let answer = 0;
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
+
+function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
